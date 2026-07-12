da148808c16e04862915e932fbb0dd80e7555319
Johan Paul <johan@paul.fi>
2026-07-12 17:14:39 +0300

    Add Firebase Cloud Function to retrieve all the active flights.

---
+++ cloud-functions/functions/src/flights.ts
@@ -0,0 +1,16 @@
+import { onGet } from "./http";
+import { getFirestore } from "firebase-admin/firestore";
+
+/**
+ * GET /overheadFlights
+ *
+ * Retrieve the overhead flights. Requires a Firebase authenticated
+ * user on the client side.
+ */
+export const overheadFlights = onGet(async (req, res) => {
+  const db = getFirestore();
+  const flightsRef = db.collection("flights");
+  const snapshot = await flightsRef.get();
+  const flights = snapshot.docs.map((doc) => doc.data());
+  res.json(flights);
+});
--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -2,3 +2,4 @@ import "./firebase"; // ensure setGlobalOptions/initializeApp run first
 
 export { token } from "./token";
 export { pushNotification } from "./push-notification";
+export { overheadFlights } from "./flights";
